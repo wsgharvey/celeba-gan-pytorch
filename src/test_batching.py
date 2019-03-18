1dbe418753fddd40ecf700f5c9910303fbd16275
Will Harvey <williamharvey08@gmail.com>
2019-03-17 19:23:27 -0700

    script to test speed up that batching gives

---
+++ src/test_batching.py
@@ -0,0 +1,21 @@
+import torch
+from dcgan import Generator
+import time
+
+CUDA_AVAILABLE = torch.cuda.is_available()
+
+
+def tb(N):
+    latents = torch.distributions.Normal(torch.zeros(1, 100), torch.ones(1, 100)).sample()
+    g = Generator()
+    if CUDA_AVAILABLE:
+        g = g.cuda()
+        latents = latents.cuda()
+
+    start = time.time()
+    [g(latents.repeat(2, 1)) for _ in range(int(N/2))]
+    print(f"{(time.time()-start)/N}s per trace with no batching")
+
+    start = time.time()
+    g(latents.repeat(N, 1))
+    print(f"{(time.time()-start)/N}s per trace with batching")
